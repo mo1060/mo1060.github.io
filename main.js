4c96e9cc40e9bcf0341d7d621b6c244823391571
mo1060 <moeggerath@gmail.com>
2017-12-05 22:44:51 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const url = 'https://mo1060.github.io/quizAPI.json';
 fetch(url)
 .then(res => res.json())
 .then(quiz => {
-  view.start.addEventListener('click tap', ()=> game.start(quiz.questions), false);
-  view.response.addEventListener('click tap', (event) => game.check(event), false);
-  view.weiter.addEventListener('click tap',(event)=> game.ask(), false);
+  view.start.addEventListener('click', ()=> game.start(quiz.questions), false);
+  view.response.addEventListener('click', (event) => game.check(event), false);
+  view.weiter.addEventListener('click',(event)=> game.ask(), false);
 });
     // Utility functions
 function random(a,b=1) {
